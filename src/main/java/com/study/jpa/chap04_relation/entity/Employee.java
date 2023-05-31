73f95470c46fdcdf35e3f2693e92ab2467f05199
Soon9 (SoonGu Hong) <naong526@naver.com>
2023-05-31 11:43:29 +0900

    # 5. 연관관계 - 양방향

--- src/main/java/com/study/jpa/chap04_relation/entity/Employee.java
+++ src/main/java/com/study/jpa/chap04_relation/entity/Employee.java
@@ -5,7 +5,8 @@ import lombok.*;
 import javax.persistence.*;
 
 @Setter @Getter
-@ToString
+// jpa연관관계 매핑에서는 연관관계 데이터는 ToString에서 제외해야 한다.
+@ToString(exclude = {"department"})
 @EqualsAndHashCode(of = "id")
 @NoArgsConstructor
 @AllArgsConstructor
@@ -22,7 +23,10 @@ public class Employee {
     @Column(name = "emp_name", nullable = false)
     private String name;
 
-    @ManyToOne
+    // EAGER: 항상 무조건 조인을 수행
+    // LAZY: 필요한 경우에만 조인을 수행 (실무)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "dept_id")
     private Department department;
+
 }
--- src/test/java/com/study/jpa/chap04_relation/repository/EmployeeRepositoryTest.java
+++ src/test/java/com/study/jpa/chap04_relation/repository/EmployeeRepositoryTest.java
@@ -96,4 +96,6 @@ class EmployeeRepositoryTest {
         System.out.println("\n\n\n");
     }
 
+
+
 }
